82b00a02b5dc696e9b70902faeccf91dd715b53e
Zhijie He <hezhijie0327@hotmail.com>
2020-08-04 21:01:05 +0800

    release.sh | Version 1.0.3
    1. <Change> Change the running logic.
    2. <Change> Cleanup data.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.0.2
+# Current Version: 1.0.3
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/DHDb.git" && chmod 0777 ./DHDb/release.sh && bash ./DHDb/release.sh
@@ -16,14 +16,14 @@ function GetData() {
 # Analyse Data
 function AnalyseData() {
     if [ ! -f "../dhdb_dead.txt" ]; then
-        dhdb_data=($(cat ./dhdb_data.tmp | sort | uniq | head -n 1000 | awk "{ print $2 }"))
+        dhdb_data=($(cat ./dhdb_data.tmp | sort | uniq | head -n 500 | awk "{ print $2 }"))
     else
         if [ ! -f "../dhdb_alive.txt" ]; then
-            dhdb_data=($(cat ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 5000 | awk "{ print $2 }"))
+            dhdb_data=($(cat ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 1500 | awk "{ print $2 }"))
         else
-            dhdb_data=($(cat ../dhdb_alive.txt ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 5000 | awk "{ print $2 }"))
+            dhdb_data=($(cat ../dhdb_alive.txt ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 1500 | awk "{ print $2 }"))
             if [ "${#dhdb_data[@]}" == 0 ]; then
-                dhdb_data=($(get_total_line=$(sed -n '$=' ../dhdb_alive.txt) && for (( tmp = 0; tmp < 2000; tmp++ )); do generate_radom_line=$(( RANDOM%${get_total_line} )); generate_radom_line=$[generate_radom_line + 1]; sed -n "$generate_radom_line"p ../dhdb_alive.txt; done > ./dhdb_alive.old && get_total_line=$(sed -n '$=' ../dhdb_dead.txt) && for (( tmp = 0; tmp < 500; tmp++ )); do generate_radom_line=$(( RANDOM%${get_total_line} )); generate_radom_line=$[generate_radom_line + 1]; sed -n "$generate_radom_line"p ../dhdb_dead.txt; done > ./dhdb_dead.old && cat ./dhdb_alive.old ./dhdb_dead.old | sort | uniq | awk "{ print $2 }"))
+                dhdb_data=($(get_total_line=$(sed -n '$=' ../dhdb_alive.txt) && for (( tmp = 0; tmp < 1000; tmp++ )); do generate_radom_line=$(( RANDOM%${get_total_line} )); generate_radom_line=$[generate_radom_line + 1]; sed -n "$generate_radom_line"p ../dhdb_alive.txt; done > ./dhdb_alive.old && get_total_line=$(sed -n '$=' ../dhdb_dead.txt) && for (( tmp = 0; tmp < 500; tmp++ )); do generate_radom_line=$(( RANDOM%${get_total_line} )); generate_radom_line=$[generate_radom_line + 1]; sed -n "$generate_radom_line"p ../dhdb_dead.txt; done > ./dhdb_dead.old && cat ./dhdb_alive.old ./dhdb_dead.old | sort | uniq | awk "{ print $2 }"))
             fi
         fi
     fi
@@ -31,25 +31,20 @@ function AnalyseData() {
 # Output Data
 function OutputData() {
     for dhdb_data_task in "${!dhdb_data[@]}"; do
-        if [ "$(dig @dns.google ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
+        if [ "$(dig A @dns.google ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ] || [ "$(dig AAAA @dns.google ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
             echo "${dhdb_data[$dhdb_data_task]} (Status: alive)"
             echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_alive.tmp
         else
-            if [ "$(dig @dns.opendns.com ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
+            if [ "$(dig A @dns.opendns.com ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ] || [ "$(dig AAAA @dns.opendns.com ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
                 echo "${dhdb_data[$dhdb_data_task]} (Status: alive)"
                 echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_alive.tmp
             else
-                if [ "$(dig @dns.quad9.net ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
+                if [ "$(dig A @one.one.one.one ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ] || [ "$(dig AAAA @one.one.one.one ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
                     echo "${dhdb_data[$dhdb_data_task]} (Status: alive)"
                     echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_alive.tmp
                 else
-                    if [ "$(dig @one.one.one.one ${dhdb_data[$dhdb_data_task]} | grep 'NXDOMAIN\|SERVFAIL\|SOA')" == "" ]; then
-                        echo "${dhdb_data[$dhdb_data_task]} (Status: alive)"
-                        echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_alive.tmp
-                    else
-                        echo "${dhdb_data[$dhdb_data_task]} (Status: dead)"
-                        echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_dead.tmp
-                    fi
+                    echo "${dhdb_data[$dhdb_data_task]} (Status: dead)"
+                    echo "${dhdb_data[$dhdb_data_task]}" >> ./dhdb_dead.tmp
                 fi
             fi
         fi
