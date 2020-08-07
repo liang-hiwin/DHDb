1800a6e52dce2f61d0edcac78c747f0b68e4ce1f
Zhijie He <hezhijie0327@hotmail.com>
2020-08-07 11:59:08 +0800

    release.sh | Version 1.1.4
    1. <Change> Change the running logic.

--- release.sh
+++ release.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Current Version: 1.1.3
+# Current Version: 1.1.4
 
 ## How to get and use?
 # git clone "https://github.com/hezhijie0327/DHDb.git" && chmod 0777 ./DHDb/release.sh && bash ./DHDb/release.sh
@@ -8,20 +8,25 @@
 ## Function
 # Get Data
 function GetData() {
+    dhdb_checklist=(
+        "https://raw.githubusercontent.com/hezhijie0327/AdFilter/master/adfilter_domains.txt"
+        "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/master/gfwlist2agh_combine.txt"
+        "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/master/trackerslist_combine.txt"
+    )
     rm -rf ./Temp && mkdir ./Temp && cd ./Temp
-    curl -s --connect-timeout 15 "https://raw.githubusercontent.com/hezhijie0327/AdFilter/master/adfilter_domains.txt" | grep -v "\#" > ./dhdb_data.tmp
-    curl -s --connect-timeout 15 "https://raw.githubusercontent.com/hezhijie0327/GFWList2AGH/master/gfwlist2agh_combine.txt" | grep "\[\/" | sed "s/\[\///g;s/\/\].*//g" >> ./dhdb_data.tmp
-    curl -s --connect-timeout 15 "https://raw.githubusercontent.com/hezhijie0327/Trackerslist/master/trackerslist_combine.txt" | sed "s/http\:\/\///g;s/https\:\/\///g;s/udp\:\/\///g;s/ws\:\/\///g;s/wss\:\/\///g;s/\:.*//g" >> ./dhdb_data.tmp
+    for dhdb_checklist_task in "${!dhdb_checklist[@]}"; do
+        curl -s --connect-timeout 15 "${dhdb_checklist[$dhdb_checklist_task]}" >> ./dhdb_checklist.tmp
+    done
 }
 # Analyse Data
 function AnalyseData() {
     if [ ! -f "../dhdb_dead.txt" ]; then
-        dhdb_data=($(cat ./dhdb_data.tmp | sort | uniq | head -n 500 | awk "{ print $2 }"))
+        dhdb_data=($(cat ./dhdb_checklist.tmp | sed "s/\[\///g;s/\/\].*//g;s/http\:\/\///g;s/https\:\/\///g;s/udp\:\/\///g;s/ws\:\/\///g;s/wss\:\/\///g;s/\:.*//g" | grep -E "^(([a-z]{1})|([a-z]{1}[a-z]{1})|([a-z]{1}[0-9]{1})|([0-9]{1}[a-z]{1})|([a-z0-9][-_\.a-z0-9]{1,61}[a-z0-9]))\.([a-z]{2,13}|[a-z0-9-]{2,30}\.[a-z]{2,3})$" | sort | uniq | head -n 500 | awk "{ print $2 }"))
     else
         if [ ! -f "../dhdb_alive.txt" ]; then
-            dhdb_data=($(cat ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 3000 | awk "{ print $2 }"))
+            dhdb_data=($(cat ./dhdb_checklist.tmp | sed "s/\[\///g;s/\/\].*//g;s/http\:\/\///g;s/https\:\/\///g;s/udp\:\/\///g;s/ws\:\/\///g;s/wss\:\/\///g;s/\:.*//g" | grep -E "^(([a-z]{1})|([a-z]{1}[a-z]{1})|([a-z]{1}[0-9]{1})|([0-9]{1}[a-z]{1})|([a-z0-9][-_\.a-z0-9]{1,61}[a-z0-9]))\.([a-z]{2,13}|[a-z0-9-]{2,30}\.[a-z]{2,3})$" > dhdb_data.tmp && cat ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 3000 | awk "{ print $2 }"))
         else
-            dhdb_data=($(cat ../dhdb_alive.txt ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 3000 | awk "{ print $2 }"))
+            dhdb_data=($(cat ./dhdb_checklist.tmp | sed "s/\[\///g;s/\/\].*//g;s/http\:\/\///g;s/https\:\/\///g;s/udp\:\/\///g;s/ws\:\/\///g;s/wss\:\/\///g;s/\:.*//g" | grep -E "^(([a-z]{1})|([a-z]{1}[a-z]{1})|([a-z]{1}[0-9]{1})|([0-9]{1}[a-z]{1})|([a-z0-9][-_\.a-z0-9]{1,61}[a-z0-9]))\.([a-z]{2,13}|[a-z0-9-]{2,30}\.[a-z]{2,3})$" > dhdb_data.tmp && cat ../dhdb_alive.txt ../dhdb_dead.txt > ./dhdb_data.old && awk 'NR == FNR { tmp[$0] = 1 } NR > FNR { if ( tmp[$0] != 1 ) print }' ./dhdb_data.old ./dhdb_data.tmp | sort | uniq | head -n 3000 | awk "{ print $2 }"))
             if (( ${#dhdb_data[@]} == 0 )); then
                 dhdb_data=($(get_total_line=$(sed -n '$=' ../dhdb_alive.txt) && for (( tmp = 0; tmp < 1200; tmp++ )); do generate_random_line=$(( RANDOM%${get_total_line} )); generate_random_line=$[generate_random_line + 1]; sed -n "$generate_random_line"p ../dhdb_alive.txt; done > ./dhdb_alive.old && get_total_line=$(sed -n '$=' ../dhdb_dead.txt) && for (( tmp = 0; tmp < 300; tmp++ )); do generate_random_line=$(( RANDOM%${get_total_line} )); generate_random_line=$[generate_random_line + 1]; sed -n "$generate_random_line"p ../dhdb_dead.txt; done > ./dhdb_dead.old && cat ./dhdb_alive.old ./dhdb_dead.old | sort | uniq | awk "{ print $2 }"))
             fi
